f0625a24f2ef988352f8b26b21a09051705ef96f
alexgordontlv <alexgordon.tlv@gmail.com>
2022-06-05 20:56:16 +0300

    added category

--- models/product.js
+++ models/product.js
@@ -1,11 +1,16 @@
+const Category = require('./category');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var ProductSchema = new Schema({
-    name: {type: String, required: true, max: 100},
-    price: {type: Number, required: true},
+var costLiving = new Schema({
+	name: { type: String, required: true, max: 100 },
+	sum: { type: Number, required: true },
+	Category: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Category',
+		required: true,
+	},
 });
 
-
 // Export the model
-module.exports = mongoose.model('Product', ProductSchema);
+module.exports = mongoose.model('CostLiving', costLiving);
