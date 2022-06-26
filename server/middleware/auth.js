137ea567aed6ead271e6da055403e6d51899c32a
alexgordontlv <alexgordon.tlv@gmail.com>
2022-06-26 21:01:53 +0300

    auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,10 @@
 //user login check
 module.exports = {
 	ensureAuth: function (req, res, next) {
-		console.log('ENSURE HEADER OK', req.headers.Authorization);
-		console.log('ENSURE HEADERs ', req.headers);
+		console.log('ENSURE HEADER OK', req.headers.authorization);
 
-		if (req.headers.Authorization) {
-			req.id = req.headers.Authorization;
+		if (req.headers.authorization) {
+			req.id = req.headers.authorization;
 			console.log('ENSURE AUTH OK', req.id);
 			return next();
 		}
